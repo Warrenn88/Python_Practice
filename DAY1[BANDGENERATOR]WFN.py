bc75376b972999cd52ade2fce19eaed77576aae3
Warren Nesteruk <warrennesteruk@gmail.com>
2024-02-17 11:38:20 -0500

    Project 1 complete

---
+++ DAY1[BANDGENERATOR]WFN.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+result=input("What's the name of the city you grew up in?")
+result2=input("What's your pet's name?")
+print("Your band name could be "+result+" "+result2)
