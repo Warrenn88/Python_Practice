06d998acc460d9c1a03aadf5df2165b0e8c84a02
warrenn88 <warrennesteruk@gmail.com>
2024-03-14 18:09:09 -0400

    second machinetest

---
+++ Blackjack Game/BlackjackSimple.py
@@ -0,0 +1,12 @@
+import random
+
+string_list = ["Welcome to Blackjack!\nDealer hits on 16.\nAce is 1 or 11.\n21 can tie."]
+cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8,
+         9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+
+
+
+
+
+
+
