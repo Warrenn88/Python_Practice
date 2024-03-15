161d0003fa0fa9f076c3f74b45e8ef0e32411348
warrenn88 <warrennesteruk@gmail.com>
2024-03-14 20:45:42 -0400

    test

--- Blackjack Game/BlackjackSimple.py
+++ Blackjack Game/BlackjackSimple.py
@@ -4,7 +4,7 @@ string_list = ["Welcome to Blackjack!\nDealer hits on 16.\nAce is 1 or 11.\n21 c
 cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8,
          9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 
-
+print("This is a test")
 
 
 
